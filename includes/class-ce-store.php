255a3341276318f14e5c76310630afedc7c4467a
kriskl-lgtm <kris.klusek@me.com>
2026-04-21 17:21:39 +0300

    Update class-ce-store.php

--- includes/class-ce-store.php
+++ includes/class-ce-store.php
@@ -20,12 +20,13 @@ class CE_Store {
         return get_option( self::OPTION, [] );
     }
 
-    public static function save( $id, $subject, $body, $enabled = true ) {
+    public static function save( $id, $subject, $body, $enabled = true, $format = 'default' ) {
         $all = get_option( self::OPTION, [] );
         $all[ $id ] = [
             'subject'  => wp_kses_post( $subject ),
             'body'     => wp_kses_post( $body ),
             'enabled'  => (bool) $enabled,
+            'format'   => in_array( $format, [ 'default', 'text', 'html' ], true ) ? $format : 'default',
             'modified' => current_time( 'mysql' ),
         ];
         update_option( self::OPTION, $all, false );
@@ -50,5 +51,17 @@ class CE_Store {
             return [ 'subject' => $override['subject'], 'body' => $override['body'] ];
         }
         return [ 'subject' => $default['default_subject'], 'body' => $default['default_body'] ];
+    }
+
+    /**
+     * Get the format for a specific email.
+     * Returns 'text', 'html', or 'default' (use global setting).
+     */
+    public static function get_format( $id ) {
+        $override = self::get( $id );
+        if ( $override && isset( $override['format'] ) ) {
+            return $override['format'];
+        }
+        return 'default';
     }
 }
